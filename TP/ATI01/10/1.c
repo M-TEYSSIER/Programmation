b0a7b69c9f5c8fd5b6af57739918e17afcb0c419
M-TEYSSIER <maxime.teyssier@laposte.net>
2018-09-24 16:33:09 +0200

    finalisation 10 point 1.c

--- TP/ATI01/10/1.c
+++ TP/ATI01/10/1.c
@@ -2,16 +2,31 @@
 
 //Somme des termes impairs de la suite de Fibonacci
 
-#define MAX 20
+#define MAX 33 
 
 int main(){
     int i;
     int tab[MAX];
+    long res,somme;
+
     tab[0]=0;
     tab[1]=1;
+    somme=res=0;
+
     for(i=2;i<MAX;i++){
-        tab[i]=tab[i-2]+tab[i-1];
-        printf("%d ",tab[i]);
+       tab[i]=tab[i-2]+tab[i-1];
+       printf("%d\n",tab[i]);
+      
+       if(tab[i]%2!=0){
+           if(tab[i]<4000000){
+              somme=res+tab[i];
+              printf("%ld \n",somme);
+              res=somme;
+           }
+      }
     }
-    printf("\n");
+   res++;
+   
+    printf("\n res FINAL: %ld\n",res);
 }
+                                            
